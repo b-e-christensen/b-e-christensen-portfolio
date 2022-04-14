c16ac1a3f6153dddc33880aaa3be578ecdf43743
Ben Christensen <ben.eric.christensen@gmail.com>
2022-04-14 11:46:10 -0600

    updated portfolio links with more recent projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 let stillDescr = document.getElementById('still-travel-h4')
-let quizDescr = document.getElementById('code-quiz-h4')
-let weatherDescr = document.getElementById('weather-dashboard-h4')
+let budgettlyDescr = document.getElementById('budgettly-h4')
+let blogDescr = document.getElementById('mvc-blog-h4')
 
 let stillAnchor = document.createElement('a')
-let quizAnchor = document.createElement('a')
-let weatherAnchor = document.createElement('a')
+let budgettlyAnchor = document.createElement('a')
+let mvcAnchor = document.createElement('a')
 
 stillAnchor.href = 'https://github.com/b-e-christensen/travel-app'
 stillAnchor.target = '_blank'
 stillAnchor.textContent = ' Click here to view repository.'
 stillAnchor.classList.add('white')
 
-quizAnchor.href = 'https://github.com/b-e-christensen/code-quiz'
-quizAnchor.target = '_blank'
-quizAnchor.textContent = ' Click here to view repository.'
-quizAnchor.classList.add('white')
+budgettlyAnchor.href = 'https://github.com/b-e-christensen/budget-tracking-app'
+budgettlyAnchor.target = '_blank'
+budgettlyAnchor.textContent = ' Click here to view repository.'
+budgettlyAnchor.classList.add('white')
 
-weatherAnchor.href = 'https://github.com/b-e-christensen/weather-dashboard'
-weatherAnchor.target = '_blank'
-weatherAnchor.textContent = ' Click here to view repository.'
-weatherAnchor.classList.add('white')
+mvcAnchor.href = 'https://github.com/b-e-christensen/mvc-tech-blog'
+mvcAnchor.target = '_blank'
+mvcAnchor.textContent = ' Click here to view repository.'
+mvcAnchor.classList.add('white')
 
 stillDescr.append(stillAnchor)
-quizDescr.append(quizAnchor)
-weatherDescr.append(weatherAnchor)
+budgettlyDescr.append(budgettlyAnchor)
+blogDescr.append(mvcAnchor)
