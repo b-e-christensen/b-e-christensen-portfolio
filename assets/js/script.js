ac4cd54f57768f21ed3f48f2680baf938272f883
Ben Christensen <ben.eric.christensen@gmail.com>
2022-06-20 15:32:10 -0600

    updated with latest project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 let stillDescr = document.getElementById('still-travel-h4')
 let budgettlyDescr = document.getElementById('budgettly-h4')
-let blogDescr = document.getElementById('mvc-blog-h4')
+let hamletDescr = document.getElementById('hamlet-h4')
 
 let stillAnchor = document.createElement('a')
 let budgettlyAnchor = document.createElement('a')
-let mvcAnchor = document.createElement('a')
+let hamletAnchor = document.createElement('a')
 
 stillAnchor.href = 'https://github.com/b-e-christensen/travel-app'
 stillAnchor.target = '_blank'
@@ -16,11 +16,11 @@ budgettlyAnchor.target = '_blank'
 budgettlyAnchor.textContent = ' Click here to view repository.'
 budgettlyAnchor.classList.add('white')
 
-mvcAnchor.href = 'https://github.com/b-e-christensen/mvc-tech-blog'
-mvcAnchor.target = '_blank'
-mvcAnchor.textContent = ' Click here to view repository.'
-mvcAnchor.classList.add('white')
+hamletAnchor.href = 'https://github.com/b-e-christensen/goals-to-do'
+hamletAnchor.target = '_blank'
+hamletAnchor.textContent = ' Click here to view repository.'
+hamletAnchor.classList.add('white')
 
 stillDescr.append(stillAnchor)
 budgettlyDescr.append(budgettlyAnchor)
-blogDescr.append(mvcAnchor)
+hamletDescr.append(hamletAnchor)
